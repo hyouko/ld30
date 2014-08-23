cc7b66f64911fd73f23e5103effd5a9ccb26bc07
hyouko <hyouko.kun@gmail.com>
2014-08-22 23:22:17 -0400

    Water and cam controls
    Implemented a basic transparent water layer effect and some simple
    keyboard-based camera controls (move + zoom)

--- conf.lua
+++ conf.lua
@@ -1,7 +1,9 @@
 function love.conf(t)
     t.window.width = 1024
     t.window.height = 768
-	t.window.title = "Ludum Dare 30 - Connected Worlds"
+	t.window.title = "Ludum Dare 30 (Connected Worlds) - Drift"
 	
 	t.console = true -- Remove this for final distro version!
 end
+
+debug_on = true
--- main.lua
+++ main.lua
@@ -1,31 +1,153 @@
 function love.load()
 	width, height = love.graphics.getDimensions()
 	
+	loadImages()
+	
 	-- Camera coords
 	cam_x = 0.0
 	cam_y = 0.0
+	
+	-- Camera velocity
+	cam_vx = 0.0
+	cam_vy = 0.0
+	
+	press_up =  false
+	press_down = false
+	press_left = false
+	press_right = false
+	
+	zoom_in = false
+	
+	zoom_out = false
+	
+	scale_factor = 1.0
+	
+	-- Universal timer
+	ticks = 0
+	
+	fps = 60
+end
+
+function loadImages()
+	
+	-- Water overlay images
+	water = {}
+	water[0] = love.graphics.newImage("gfx/water_01.png")
+	
 end
 
 function love.draw()
 	
-	love.graphics.setColor(10, 10, 10)
+	love.graphics.setColor(10, 100, 210)
 	love.graphics.rectangle('fill', 0, 0, width, height)
 	
-	love.graphics.setColor(180, 180, 180)
-	love.graphics.print('Hello, world!', 0, 0)
+	drawWaterLayer(30, 20, 0)
+	drawWaterLayer(-10, 30, math.pi / 2)
+	drawWaterLayer(15, 0, math.pi * 1.5)
+	
+	if debug_on then
+		love.graphics.setColor(250, 250, 250)
+		love.graphics.print("FPS: " .. fps, 20, 20)
+		love.graphics.print("CamX: " .. math.ceil(cam_x  * 10) / 10, 20, 40)
+		love.graphics.print("CamY: " .. math.ceil(cam_y  * 10) / 10, 120, 40)
+	end
+end
+
+function drawWaterLayer(xvel, yvel, r)
+	
+	x_offset = (xvel * ticks - cam_x) % 128
+	y_offset = (yvel * ticks - cam_y) % 128
+	
+	for x = (x_offset - 256) * scale_factor, width + (x_offset + 256) * scale_factor, 128 * scale_factor do
+		for y = (y_offset - 256) * scale_factor, height + (y_offset + 256) * scale_factor, 128 * scale_factor do
+			love.graphics.draw(water[0], x, y, r, scale_factor, scale_factor)	
+		end
+	end
 end
 
 
 function love.update(dt)
+	ticks = ticks + dt
 	
+	--scale_factor = math.sin(ticks) * 0.25 + 1.0
+	
+	cam_x = cam_x + cam_vx
+	cam_y = cam_y + cam_vy
+	
+	
+	-- Accelerate camera based on inputs
+	if press_up then
+		cam_vy = cam_vy - 1
+	end
+	
+	if press_down then
+		cam_vy = cam_vy + 1
+	end
+	
+	if press_left then
+		cam_vx = cam_vx - 1
+	end
+	
+	if press_right then
+		cam_vx = cam_vx + 1
+	end
+	
+	-- Zoom in / out based on inputs
+	if zoom_in then
+		scale_factor = math.min(1.5, scale_factor * 1.005)
+	end
+	
+	if zoom_out then
+		scale_factor = math.max(0.5, scale_factor * 0.995)
+	end
+	
+	-- Decelerate camera
+	if math.abs(cam_vx) > 0.2 then
+		cam_vx = math.min(cam_vx * 0.9, 5)
+	else
+		cam_vx = 0
+	end
+	
+	if math.abs(cam_vy) > 0.2 then
+		cam_vy = math.min(cam_vy * 0.9, 5)
+	else
+		cam_vy = 0
+	end
+	
+	
+	fps = math.ceil((1 / dt + fps * 29) / 3) / 10
 end
 
 function love.keypressed(key)
-
+	if key == "s" or key == "down" then
+		press_down = true
+	elseif key == "w" or key == "up" then
+		press_up = true
+	elseif key == "a" or key == "left" then
+		press_left = true
+	elseif key == "d" or key == "right" then
+		press_right = true
+	elseif key == "q" then
+		zoom_in = true
+	elseif key == "e" then
+		zoom_out = true
+	end
 end
 
 function love.keyreleased(key)
-	
+	if key == "s" or key == "down" then
+		press_down = false
+	elseif key == "w" or key == "up" then
+		press_up = false
+	elseif key == "a" or key == "left" then
+		press_left = false
+	elseif key == "d" or key == "right" then
+		press_right = false
+	elseif key == "q" then
+		zoom_in = false
+	elseif key == "e" then
+		zoom_out = false
+	end
 end
 
 function love.mousepressed(x, y, button)
